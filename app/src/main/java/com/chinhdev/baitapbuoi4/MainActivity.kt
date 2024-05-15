0f92f7d02ef57c4c34129a155ac29f8d8db54ae8
chinhdev <chinhtdph40493@fpr.edu.vn>
2024-05-15 22:29:26 +0700

    Initial commit

---
+++ app/src/main/java/com/chinhdev/baitapbuoi4/MainActivity.kt
@@ -0,0 +1,89 @@
+package com.chinhdev.baitapbuoi4
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.chinhdev.baitapbuoi4.ui.theme.BaiTapBuoi4Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GreetingCard("Tống Doanh Chính - PH40493")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(
+        text = "Hello, $name!",
+        modifier = Modifier
+            .padding(0.dp, 16.dp)
+            .fillMaxWidth(),
+        color = Color.DarkGray,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewGreeting() {
+    GreetingCard("Tống Doanh Chính - PH40493")
+}
+
+@Composable
+fun GreetingCard(msg: String) {
+    var text by remember { mutableStateOf(msg) }
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        MessageCard(msg = text)
+        Button(onClick = { text = "Hi!" }) {
+            Text("Say Hi!")
+        }
+    }
+}
+
+@Composable
+fun MessageCard(msg: String) {
+    Text(
+        text = msg,
+        modifier = Modifier
+            .padding(0.dp, 16.dp)
+            .fillMaxWidth(),
+        color = Color.DarkGray,
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold,
+        textAlign = TextAlign.Center,
+    )
+}
